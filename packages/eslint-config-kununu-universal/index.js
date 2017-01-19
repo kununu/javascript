54817b2d953bb5c6b3c7797d41e4f7254a27d66a
Karl Horky <karl.horky@gmail.com>
2017-01-19 20:55:32 +0100

    Add universal ESLint config package

---
+++ packages/eslint-config-kununu-universal/index.js
@@ -0,0 +1,36 @@
+module.exports = {
+  extends: 'karlhorky',
+  rules: {
+    'class-methods-use-this': ['error', {
+      exceptMethods: [
+        // React lifecycle methods
+        // https://github.com/airbnb/javascript/blob/8468ed842314a5d66816927ba6c35f018035cffc/packages/eslint-config-airbnb/rules/react.js#L23-L33
+        'render',
+        'getInitialState',
+        'getDefaultProps',
+        'getChildContext',
+        'componentWillMount',
+        'componentDidMount',
+        'componentWillReceiveProps',
+        'shouldComponentUpdate',
+        'componentWillUpdate',
+        'componentDidUpdate',
+        'componentWillUnmount',
+
+        // React Server methods
+        'handleRoute',
+        'getTitle',
+        'getScripts',
+        'getSystemScripts',
+        'getHeadStylesheets',
+        'getMetaTags',
+        'getLinkTags',
+        'getBase',
+        'getBodyClasses',
+        'getElements',
+        'getResponseData',
+        'getHttpHeaders',
+      ],
+    }
+  }
+};
