fe5a7de693776b7ab990cb02a1fad66b71fd1ef6
Ricardo Neves <ricardofernandodasneves@gmail.com>
2021-08-25 17:34:25 +0100

    build(hooks): Fixed build script to run with root build bash.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env sh
 
-
 dist_folder="dist"
 
 rm -rf $dist_folder
-babel --extensions \".ts,.tsx\" src --out-dir $dist_folder --ignore 'src/**/*.spec.tsx'
+babel --extensions '.ts,.tsx' src --out-dir $dist_folder --ignore 'src/**/*.spec.tsx'
