a278ab153a028fbbfdf74d3ee05873017371f9f9
Patricia Sousa <patricia.sousa@kununu.com>
2021-08-31 12:00:42 +0100

    improve test coverage

--- packages/utils/src/utils/api/index.spec.ts
+++ packages/utils/src/utils/api/index.spec.ts
@@ -1,5 +1,6 @@
 import fetchMock from 'fetch-mock';
 import * as kununuSessionCallback from '@kununu/kununu-utils/dist/kununu-helpers/kununuSession';
+import {logger} from '@kununu/kununu-utils/dist/kununu-logger';
 
 import fetchApiDomain, {getBFFURL} from '../fetchApiDomain';
 
@@ -10,6 +11,13 @@ jest.mock('../isClientRender', () => () => true);
 jest.mock('next/config', () => () => ({
   publicRuntimeConfig: {
     server: {name: 'app-profiles'},
+  },
+}));
+
+jest.mock('@kununu/kununu-utils/dist/kununu-logger', () => ({
+  logger: {
+    error: jest.fn(),
+    info: jest.fn(),
   },
 }));
 
@@ -104,6 +112,57 @@ describe('Utils', () => {
       const result = await fetchApi(endpoint, {someparam: 'set'})();
 
       expect(result).toEqual(body);
+    });
+
+    it('calls logger info beacause window is undefined', async () => {
+      delete global.window;
+      const endpoint = '/queryparams';
+      const body = {message: 'some param was set'};
+      const url = `${fetchApiDomain()}${getBFFURL(endpoint)}?someparam=sete`;
+
+      fetchMock.once(url, {
+        body,
+      });
+
+      await fetchApi(endpoint, {someparam: 'sete'})();
+
+      expect(logger.info).toHaveBeenCalled();
+    });
+
+    it('calls logger error when request takes too long and window is undefined', async () => {
+      delete global.window;
+      const endpoint = '/timeout1';
+      const url = `${fetchApiDomain()}${getBFFURL(endpoint)}`;
+
+      fetchMock.once(url, () => new Promise(resolve => {
+        setTimeout(() => {
+          resolve('');
+        }, 200);
+      }));
+
+      try {
+        await fetchApi(endpoint, {}, {}, 100)();
+      } catch (err) {
+        expect(err.status).toEqual(504);
+      }
+    });
+
+    it('should call reject because 406 is not an ok status', async () => {
+      const endpoint = '/reject';
+      const url = `${fetchApiDomain()}${getBFFURL(endpoint)}`;
+      const body = {message: 'ok'};
+
+      fetchMock.once(url, {
+        body,
+      }, {response: {status: 406}});
+
+      await expect(fetchApi(endpoint)).rejects.toEqual({
+        response: {
+          _abort: false, _fmResults: {}, _raw: [], body: undefined, bodyUsed: false, headers: {_headers: {}}, ok: false, size: 0, status: 406, statusText: 'Not Acceptable', timeout: 0, url: '/middlewares/reject',
+        },
+        status: 406,
+        statusText: 'Not Acceptable',
+      });
     });
   });
 });
