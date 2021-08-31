c4eeee9ba4d4ff5f289cead2ddf4deb97d72dfbc
Patricia Sousa <patricia.sousa@kununu.com>
2021-08-31 08:44:31 +0100

    implement language util

---
+++ packages/utils/src/utils/languages/index.spec.ts
@@ -0,0 +1,416 @@
+/* eslint-disable global-require */
+/* eslint-disable @typescript-eslint/no-var-requires */
+import {join} from 'path';
+
+import {serverSideGetTranslations, clientSideGetTranslations, getLanguage} from '.';
+
+const defaultReq = {
+  cookies: {},
+  headers: {},
+  originalUrl: '/at/kununu',
+  query: {},
+};
+
+const res = {
+  cookie: jest.fn(),
+  redirect: jest.fn(),
+};
+
+describe('Get languages', () => {
+  afterEach(() => {
+    res.redirect.mockClear();
+  });
+
+  describe('URLs don\'t have x-lang query parameter defined', () => {
+    it('should return de_DE because url don\'t have country defined', () => {
+      const req = {
+        ...defaultReq,
+        originalUrl: '/',
+      };
+
+      expect(getLanguage(req, res)).toEqual({hrefLang: 'de-DE', language: 'de', locale: 'de_DE'});
+    });
+
+    it('should return de_DE because is the current country', () => {
+      const req = {
+        ...defaultReq,
+        originalUrl: '/de/cgi-deutschland',
+      };
+
+      expect(getLanguage(req, res)).toEqual({hrefLang: 'de-DE', language: 'de', locale: 'de_DE'});
+    });
+
+    it('should return de_DE because AT is the current country', () => {
+      expect(getLanguage(defaultReq, res)).toEqual({hrefLang: 'de-DE', language: 'de', locale: 'de_DE'});
+    });
+
+    it('should return de_DE as default because PT isn\'t a valid country', () => {
+      const req = {
+        ...defaultReq,
+        originalUrl: '/pt/kununu',
+      };
+
+      expect(getLanguage(req, res)).toEqual({hrefLang: 'de-DE', language: 'de', locale: 'de_DE'});
+    });
+
+    it('should return de_DE because cp it isn\'t a valid country', () => {
+      const req = {
+        ...defaultReq,
+        originalUrl: '/cp',
+      };
+
+      expect(getLanguage(req, res)).toEqual({hrefLang: 'de-DE', language: 'de', locale: 'de_DE'});
+    });
+
+    it('should not set cookie value because x-lang query parameter its not defined', () => {
+      getLanguage(defaultReq, res);
+      expect(res.cookie).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('URLs have x-lang query parameter defined', () => {
+    it('should return en_US because x-lang value is supported', () => {
+      const req = {
+        ...defaultReq,
+        originalUrl: '/de/cgi-deutschland?x-lang=en_US',
+        query: {
+          'x-lang': 'en_US',
+        },
+      };
+
+      expect(getLanguage(req, res)).toEqual({hrefLang: 'en-US', language: 'en', locale: 'en_US'});
+    });
+
+    it('should use language of URL countryCode, because x-lang don\'t have a valid value and neither cookies or acceptance headers are defined', () => {
+      const req = {
+        ...defaultReq,
+        originalUrl: '/at/kununu?x-lang=',
+        query: {
+          'x-lang': '',
+        },
+      };
+
+      expect(getLanguage(req, res)).toEqual({hrefLang: 'de-DE', language: 'de', locale: 'de_DE'});
+    });
+
+    it('should use language of URL countryCode, because x-lang don\'t have a supported value and neither cookies or acceptance headers are defined', () => {
+      const req = {
+        ...defaultReq,
+        originalUrl: '/at/kununu?x-lang=pt_PT',
+        query: {
+          'x-lang': '',
+        },
+      };
+
+      expect(getLanguage(req, res)).toEqual({hrefLang: 'de-DE', language: 'de', locale: 'de_DE'});
+    });
+
+    it('should use de_DE, because x-lang have DE defined as language', () => {
+      const req = {
+        ...defaultReq,
+        originalUrl: '/at/kununu?x-lang=de',
+        query: {
+          'x-lang': 'de',
+        },
+      };
+
+      expect(getLanguage(req, res)).toEqual({hrefLang: 'de-DE', language: 'de', locale: 'de_DE'});
+    });
+
+    it('should use de_DE, because x-lang have an invalid language defined', () => {
+      const req = {
+        ...defaultReq,
+        originalUrl: '/at/kununu?x-lang=pt',
+        query: {
+          'x-lang': 'pt',
+        },
+      };
+
+      expect(getLanguage(req, res)).toEqual({hrefLang: 'de-DE', language: 'de', locale: 'de_DE'});
+    });
+
+    it('should use de_DE, because x-lang have a german locale', () => {
+      const req = {
+        ...defaultReq,
+        originalUrl: '/at/kununu?x-lang=de_AT',
+        query: {
+          'x-lang': 'de_AT',
+        },
+      };
+
+      expect(getLanguage(req, res)).toEqual({hrefLang: 'de-DE', language: 'de', locale: 'de_DE'});
+    });
+
+    it('should redirect because the valid x-lang is different from the url x-lang', () => {
+      const req = {
+        ...defaultReq,
+        originalUrl: '/de/cgi-deutschland?x-lang=de_AT',
+        query: {
+          'x-lang': 'de_AT',
+        },
+      };
+
+      getLanguage(req, res);
+      expect(res.redirect).toHaveBeenCalledWith(302, '/de/cgi-deutschland?x-lang=de_DE');
+    });
+
+    it('should redirect because x-lang the url x-lang is the short version', () => {
+      const req = {
+        ...defaultReq,
+        originalUrl: '/de/cgi-deutschland?x-lang=de',
+        query: {
+          'x-lang': 'de',
+        },
+      };
+
+      getLanguage(req, res);
+      expect(res.redirect).toHaveBeenCalledWith(302, '/de/cgi-deutschland?x-lang=de_DE');
+    });
+
+    it('should not redirect because x-lang the url x-lang is valid', () => {
+      const req = {
+        ...defaultReq,
+        originalUrl: '/de/cgi-deutschland?x-lang=de_DE',
+        query: {
+          'x-lang': 'de_DE',
+        },
+      };
+
+      getLanguage(req, res);
+      expect(res.redirect).not.toHaveBeenCalled();
+    });
+
+    it('should set cookie value because URL have x-lang query parameter is defined', () => {
+      const req = {
+        ...defaultReq,
+        originalUrl: '/de/cgi-deutschland?x-lang=en_US',
+        query: {
+          'x-lang': 'en_US',
+        },
+      };
+
+      getLanguage(req, res);
+      expect(res.cookie).toHaveBeenCalled();
+    });
+  });
+
+  describe('URLs have x-lang query parameter defined and cookie defined', () => {
+    const defaultCookieReq = {
+      ...defaultReq,
+      originalUrl: '/at/kununu?x-lang=',
+      query: {
+        'x-lang': '',
+      },
+    };
+
+    it('should return en_US because url have x-lang defined in cookie', () => {
+      const req = {
+        ...defaultCookieReq,
+        cookies: {
+          kununu_x_lang: 'en_US',
+        },
+      };
+
+      expect(getLanguage(req, res)).toEqual({hrefLang: 'en-US', language: 'en', locale: 'en_US'});
+    });
+
+    it('should use en_US, because en is the locale supported for en language', () => {
+      const req = {
+        ...defaultCookieReq,
+        cookies: {
+          kununu_x_lang: 'en_GB',
+        },
+      };
+
+      expect(getLanguage(req, res)).toEqual({hrefLang: 'en-US', language: 'en', locale: 'en_US'});
+    });
+
+    it('should use en, because en is the kununu_x_lang value defined', () => {
+      const req = {
+        ...defaultCookieReq,
+        cookies: {
+          kununu_x_lang: 'en',
+        },
+      };
+
+      expect(getLanguage(req, res)).toEqual({hrefLang: 'en-US', language: 'en', locale: 'en_US'});
+    });
+
+    it('should use de_DE, because pt is the kununu_x_lang value defined and is not supported', () => {
+      const req = {
+        ...defaultCookieReq,
+        cookies: {
+          kununu_x_lang: 'pt',
+        },
+      };
+
+      expect(getLanguage(req, res)).toEqual({hrefLang: 'de-DE', language: 'de', locale: 'de_DE'});
+    });
+
+    it('should redirect because the valid x-lang is different from the url x-lang', () => {
+      const req = {
+        ...defaultCookieReq,
+        cookies: {
+          kununu_x_lang: 'en_GB',
+        },
+      };
+
+      getLanguage(req, res);
+      expect(res.redirect).toHaveBeenCalledWith(302, '/at/kununu?x-lang=en_US');
+    });
+
+    it('should set cookie value because URL have x-lang query parameter is defined and cookie is also defined', () => {
+      const req = {
+        ...defaultCookieReq,
+        cookies: {
+          kununu_x_lang: 'en_GB',
+        },
+      };
+
+      getLanguage(req, res);
+      expect(res.cookie).toHaveBeenCalled();
+    });
+  });
+
+  describe('URLs have x-lang query parameter defined and cookie defined and Acceptance header defined', () => {
+    const defaultHeaderReq = {
+      ...defaultReq,
+      cookies: {
+        kununu_x_lang: 'pt_PT',
+      },
+      headers: {
+        'accept-language': 'en-US,en;q=0.9,en-GB;q=0.8,pt;q=0.7',
+      },
+      originalUrl: '/at/kununu?x-lang=',
+      query: {
+        'x-lang': '',
+      },
+    };
+
+    it('should use en_US because it\'s defined in the accept language headers', () => {
+      const req = {
+        ...defaultHeaderReq,
+        headers: {
+          'accept-language': 'en-US,en;q=0.9,en-GB;q=0.8,pt;q=0.7',
+        },
+      };
+
+      expect(getLanguage(req, res)).toEqual({hrefLang: 'en-US', language: 'en', locale: 'en_US'});
+    });
+
+    it('should use de_DE because de_AT it\'s defined in the accept language headers', () => {
+      const req = {
+        ...defaultHeaderReq,
+        headers: {
+          'accept-language': 'de-AT,en;q=0.9,De-DE;q=0.8,pt;q=0.7',
+        },
+      };
+
+      expect(getLanguage(req, res)).toEqual({hrefLang: 'de-DE', language: 'de', locale: 'de_DE'});
+    });
+
+    it('should use en_US because en-GB it\'s defined in the accept Language headers', () => {
+      const req = {
+        ...defaultHeaderReq,
+        headers: {
+          'accept-language': 'en-GB',
+        },
+      };
+
+      expect(getLanguage(req, res)).toEqual({hrefLang: 'en-US', language: 'en', locale: 'en_US'});
+    });
+
+    it('should use de_DE because pt_PT it\'s not supported', () => {
+      const req = {
+        ...defaultHeaderReq,
+        headers: {
+          'accept-language': 'pt-PT',
+        },
+      };
+
+      expect(getLanguage(req, res)).toEqual({hrefLang: 'de-DE', language: 'de', locale: 'de_DE'});
+    });
+
+    it('should use en_US because en it\'s defined in the accept Language headers', () => {
+      const req = {
+        ...defaultHeaderReq,
+        headers: {
+          'accept-language': 'pt-PT,  en;q=0.5',
+        },
+      };
+
+      expect(getLanguage(req, res)).toEqual({hrefLang: 'en-US', language: 'en', locale: 'en_US'});
+    });
+
+    it('should use de_DE because * it\'s defined in the accept Language headers and we fallback to countryCode in URL', () => {
+      const req = {
+        ...defaultHeaderReq,
+        headers: {
+          'accept-language': 'pt-PT,  *;q=0.5',
+        },
+        originalUrl: '?x-lang=',
+      };
+
+      expect(getLanguage(req, res)).toEqual({hrefLang: 'de-DE', language: 'de', locale: 'de_DE'});
+    });
+
+    it('should use de_DE because nothing is defined in the accept Language headers is supported', () => {
+      const req = {
+        ...defaultHeaderReq,
+        headers: {
+          'accept-language': 'pt-PT,fr-CA',
+        },
+      };
+
+      expect(getLanguage(req, res)).toEqual({hrefLang: 'de-DE', language: 'de', locale: 'de_DE'});
+    });
+
+    it('should use de_DE because nothing is support in Accept header and it contains a script', () => {
+      const req = {
+        ...defaultHeaderReq,
+        headers: {
+          'accept-language': 'zh-Hant-cn;q=1, zh-cn;q=0.6, zh;q=0.4',
+        },
+      };
+
+      expect(getLanguage(req, res)).toEqual({hrefLang: 'de-DE', language: 'de', locale: 'de_DE'});
+    });
+
+    it('should redirect because the valid x-lang is different from the url x-lang', () => {
+      const req = {
+        ...defaultHeaderReq,
+        headers: {
+          'accept-language': 'en-GB',
+        },
+      };
+
+      getLanguage(req, res);
+      expect(res.redirect).toHaveBeenCalledWith(302, '/at/kununu?x-lang=en_US');
+    });
+
+    it('should set cookie value because URL have x-lang query parameter is defined and cookie is defined and Acceptance header is also defined', () => {
+      const req = {
+        ...defaultHeaderReq,
+        headers: {
+          'accept-language': 'zh-Hant-cn;q=1, zh-cn;q=0.6, zh;q=0.4',
+        },
+      };
+
+      getLanguage(req, res);
+      expect(res.cookie).toHaveBeenCalled();
+    });
+  });
+});
+
+describe('Get messages', () => {
+  it('gets the correct translations for each language in server side', () => {
+    /* eslint-disable import/no-dynamic-require */
+    expect(require(join(__dirname, '../../../__mocks__/translationMock-en_US.json'))).toEqual(serverSideGetTranslations('en_US', 'translationMock'));
+    expect(require(join(__dirname, '../../../__mocks__/translationMock-de_DE.json'))).toEqual(serverSideGetTranslations('de_DE', 'translationMock'));
+    /* eslint-enable import/no-dynamic-require */
+  });
+
+  it('gets the correct translations for each language in client side', () => {
+    expect(import('../../../__mocks__/translationMock-en_US.json')).toEqual(clientSideGetTranslations('en_US', 'translationMock'));
+  });
+});
---
+++ packages/utils/src/utils/languages/index.ts
@@ -0,0 +1,123 @@
+import {join} from 'path';
+
+import setCookieLanguage from '../setCookieLanguage';
+
+type ReqProp = {
+  query: Record<string, string>,
+  originalUrl: string,
+  cookies: Record<string, string>,
+  headers: Record<string, string>,
+};
+
+type ResProp = {
+
+}
+
+const languages = {
+  de: {
+    countries: ['at', 'ch', 'de'],
+    default: 'de_DE',
+  },
+  en: {
+    countries: ['us'],
+    default: 'en_US',
+  },
+};
+const supportedLanguageCodes = Object.keys(languages);
+const defaultLocale = languages.de.default;
+
+// https://github.com/opentable/accept-language-parser
+const parseAcceptLanguage = al => al.replace(/ /g, '').split(',').map(m => {
+  const bits = m.split(';');
+  const ietf = bits[0].split('-');
+  const hasScript = ietf.length === 3;
+
+  return {
+    code: ietf[0],
+    quality: bits[1] ? parseFloat(bits[1].split('=')[1]) : 1.0,
+    region: hasScript ? ietf[2] : ietf[1],
+    script: hasScript ? ietf[1] : null,
+  };
+}).sort((a, b) => b.quality - a.quality);
+
+const getLocaleByUrl = originalUrl => {
+  const match = originalUrl.match(/^\/(?<countryCode>[a-z]{2})(\/|$)/);
+  const countryCode = match && match.groups ? match.groups.countryCode : undefined;
+
+  // case if url don't have a countryCode
+  if (!countryCode) return defaultLocale;
+  const countryIsLanguage = supportedLanguageCodes.includes(countryCode);
+
+  // case if countryCode is a supported language
+  if (countryIsLanguage) return languages[countryCode].default;
+
+  const language = supportedLanguageCodes.find(lang => languages[lang].countries.includes(countryCode));
+
+  // case if countryCode is supported  by a language
+  if (language) return languages[language].default;
+
+  // default to german
+  return defaultLocale;
+};
+
+const setLanguageObj = (locale: string, req: ReqProp, res?: any) => {
+  const {'x-lang': xLangQuery} = req.query;
+
+  if (res) setCookieLanguage(locale, res);
+
+  const response = {
+    hrefLang: locale.replace('_', '-'),
+    language: locale.split('_')[0],
+    locale,
+  };
+
+  if (typeof xLangQuery !== 'undefined' && response.locale !== xLangQuery) res.redirect(302, req.originalUrl.replace(/x-lang.*?(?=(?:&|$))/, `x-lang=${response.locale}`));
+
+  return response;
+};
+
+const handleXLang = (xLangQuery: string, req: ReqProp, res: unknown) => {
+  // To prevent the case where the user defined the x-lang in the URL, but didn't pass any value
+  if (xLangQuery) {
+    const languageInXlang = xLangQuery.split('_')[0];
+
+    if (supportedLanguageCodes.includes(languageInXlang)) return setLanguageObj(languages[languageInXlang].default, req, res);
+  }
+  return null;
+};
+
+const handleAcceptLanguage = (acceptLanguage: string, req: ReqProp, res: unknown, localeInURL: string) => {
+  if (acceptLanguage) {
+    const languageSupported = parseAcceptLanguage(acceptLanguage)
+      .find(al => supportedLanguageCodes.includes(al.code) || al.code === '*' || supportedLanguageCodes.includes(al.code.split('-')[0]));
+
+    const {code} = languageSupported || {};
+    const acceptAll = code === '*';
+
+    if (!code || acceptAll) return setLanguageObj(localeInURL, req, res);
+
+    return setLanguageObj(languages[code].default, req, res);
+  }
+  return null;
+};
+
+export const getLanguage = (req: ReqProp, res: unknown): unknown => {
+  const {'x-lang': xLangQuery} = req.query;
+  const {kununu_x_lang: xLangCookie} = req.cookies;
+  const {'accept-language': acceptLanguage} = req.headers;
+
+  const localeInURL = getLocaleByUrl(req.originalUrl);
+
+  // because ?x-lang and ?x-lang= are valid cases
+  if (typeof xLangQuery === 'undefined') return setLanguageObj(localeInURL, req); // don't pass res here, because we don't want to set the cookie
+
+  return handleXLang(xLangQuery, req, res) ||
+    handleXLang(xLangCookie, req, res) ||
+    handleAcceptLanguage(acceptLanguage, req, res, localeInURL) ||
+    setLanguageObj(localeInURL, req, res);
+};
+// eslint-disable-next-line
+export const serverSideGetTranslations = (locale, page) => require(join(__dirname, `../../client/translations/${page}-${locale}.json`));
+
+// I had to switch here to normal string concat because jests watch mode was constantly complaining about not finding dynamic async imports …
+export const clientSideGetTranslations = (locale, page) => import(/* webpackChunkName: '[request]' */ 'client/translations/' + page + '-' + locale + '.json'); // eslint-disable-line prefer-template
