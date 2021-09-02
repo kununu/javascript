6605e3bff18ddf5dae03d10e75965bed455140b5
Jacinto Rodrigues <jacinto.rodrigues@kununu.com>
2021-09-02 21:52:22 +0100

    Add type

--- packages/redux/src/actions/userAgent/index.ts
+++ packages/redux/src/actions/userAgent/index.ts
@@ -1,5 +1,5 @@
 export const SET_USER_AGENT = 'SET_USER_AGENT';
 
-export function setUserAgent (ua): Record<string, any> {
+export function setUserAgent (ua: string): Record<string, any> {
   return {payload: ua, type: SET_USER_AGENT};
 }
