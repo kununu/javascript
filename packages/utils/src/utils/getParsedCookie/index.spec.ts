af75693e08d8df60bb8fd5efb158010e08d94fba
Jacinto Rodrigues <jacinto.rodrigues@kununu.com>
2021-09-03 10:15:02 +0100

    remove unnecessary describes

--- packages/utils/src/utils/getParsedCookie/index.spec.ts
+++ packages/utils/src/utils/getParsedCookie/index.spec.ts
@@ -1,39 +1,36 @@
 import getParsedCookie from '.';
 
-describe('Server', () => {
-  describe('utils', () => {
-    describe('getParsedCookie', () => {
-      const application = 'app-sitemaps';
+describe('getParsedCookie', () => {
+  const application = 'app-sitemaps';
 
-      it('parses a valid stringified cookie', () => {
-        const myCookieData = {
-          data: 'stringifiedData',
-        };
+  it('parses a valid stringified cookie', () => {
+    const myCookieData = {
+      data: 'stringifiedData',
+    };
 
-        const cookies = {
-          myCookie: JSON.stringify(myCookieData),
-        };
+    const cookies = {
+      myCookie: JSON.stringify(myCookieData),
+    };
 
-        expect(getParsedCookie(cookies, 'myCookie', application)).toEqual(myCookieData);
-      });
+    expect(getParsedCookie(cookies, 'myCookie', application)).toEqual(myCookieData);
+  });
 
-      it('returns null, when the cookie does not exist', () => {
-        const cookies = {};
+  it('returns null, when the cookie does not exist', () => {
+    const cookies = {};
 
-        expect(getParsedCookie(cookies, 'myCookie', application)).toEqual(null);
-      });
+    expect(getParsedCookie(cookies, 'myCookie', application)).toEqual(null);
+  });
 
-      it('returns null, when the cookie cannot be parsed', () => {
-        const cookies = {
-          myCookie: 'some { invalid json',
-        };
+  it('returns null, when the cookie cannot be parsed', () => {
+    const cookies = {
+      myCookie: 'some { invalid json',
+    };
 
-        try {
-          expect(getParsedCookie(cookies, 'myCookie', application)).toEqual(null);
-        } catch (err) {
-          expect(err).not.toBe(undefined);
-        }
-      });
-    });
+    try {
+      expect(getParsedCookie(cookies, 'myCookie', application)).toEqual(null);
+    } catch (err) {
+      expect(err).not.toBe(undefined);
+    }
   });
 });
+
