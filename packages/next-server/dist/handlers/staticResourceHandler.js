0b1a2afd78aa4760f6cf81b83fd46c7dc5903fe0
hyorranKununu <90780986+hyorranKununu@users.noreply.github.com>
2021-12-07 16:40:25 +0000

    removed trash

--- packages/next-server/dist/handlers/staticResourceHandler.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var staticResourceHandler = function (req, res, nextHandler) {
-    res.header('cache-control', 'public, max-age=31536000, immutable');
-    nextHandler();
-};
-exports.default = staticResourceHandler;
